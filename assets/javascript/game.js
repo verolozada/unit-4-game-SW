a55984072ad240dab80c9257a74fd52997f0037f
Veronica L <verolozada12@gmail.com>
2018-09-11 20:46:35 -0400

    images class added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,14 @@
 $("#c1-button").on("click", function (){
     // alert("I've been CLICKED!");
     // var string = "You chose Obi"
-
     var newDiv = $("<div>")
-    .attr("id", "newDiv")
-    .html("<img src='https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/01/30/11/Ewan-McGregor-as-Obi-Wan-014.jpg' width= 150px, height=100px>");
+    .html("<img class = players src='https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/01/30/11/Ewan-McGregor-as-Obi-Wan-014.jpg'>");
     $("#character").append(newDiv);
 
+    var newDiv1= $("<div>")
+    .html("<img class= payers src ='https://www.sideshowtoy.com/wp-content/uploads/2017/07/star-wars-luke-skywalker-sixth-scale-hot-toys-feature-903109.jpg'> <img class= payers src ='https://www.sideshowtoy.com/wp-content/uploads/2017/07/star-wars-luke-skywalker-sixth-scale-hot-toys-feature-903109.jpg'>");
+    $("#enemies").append(newDiv1);
 
 });
+
 
