2489406faf9d6d67aca67968feffe29fe3d85bc0
Veronica L <verolozada12@gmail.com>
2018-09-13 20:24:36 -0400

    .enemies not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,73 @@
+window.onload = function () {
 
-function obi() { 
-$("#c1-button").on("click", function (){
-    // alert("I've been CLICKED!");
-    // var string = "You chose Obi"
-    var newDiv = $("<div>")
-    .html("<img class = players src='https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/01/30/11/Ewan-McGregor-as-Obi-Wan-014.jpg'>");
-    $("#character").append(newDiv);
+    var characters = [{
+        name: "Obi",
+        attackPoints: 8,
+        healthPoint: 50,
+        deffendPoints: 5,
+        img: "assets/images/Obi.jpg"
+    }, {
+        name: "Luke",
+        attackPoints: 10,
+        healthPoint: 40,
+        deffendPoints: 6,
+        img: "https://www.sideshowtoy.com/wp-content/uploads/2017/07/star-wars-luke-skywalker-sixth-scale-hot-toys-feature-903109.jpg"
+    }, {
+        name: "Darth",
+        attackPoints: 3,
+        healthPoint: 70,
+        deffendPoints: 15,
+        img: "assets/images/Darth.jpg"
+    }, {
+        name: "Battlefront",
+        attackPoints: 3,
+        healthPoint: 70,
+        deffendPoints: 15,
+        img: "assets/images/Battlefront.png"
+    }];
 
-    var newDiv1= $("<div>")
-    .html("<img class= players src ='https://www.sideshowtoy.com/wp-content/uploads/2017/07/star-wars-luke-skywalker-sixth-scale-hot-toys-feature-903109.jpg'> <img class= players src ='https://www.sideshowtoy.com/wp-content/uploads/2017/07/star-wars-luke-skywalker-sixth-scale-hot-toys-feature-903109.jpg'><img class= players src ='https://www.sideshowtoy.com/wp-content/uploads/2017/07/star-wars-luke-skywalker-sixth-scale-hot-toys-feature-903109.jpg'>");
-    $("#enemies").append(newDiv1);
+    var chosenCharacter;
+    var enemies = [];
+    var attacker;
 
-});
+    $(".players").on("click", function () {
+        var charName = $(this).attr("data-name");
+        $(".players").hide();
+        console.log("charName: " + charName);
+        for (var i = 0; i < characters.length; i++) {
+            if (characters[i].name === charName) {
+                var image = $("<img>")
+                image.attr("src", characters[i].img)
+                image.addClass("chars")
+                $("#character").append(image)
+                chosenCharacter = characters[i];
+                console.log(chosenCharacter);
+            } else {
+                enemies.push(characters[i]);
+                var image = $("<img>")
+                image.attr("src", characters[i].img)
+                image.attr("data-name", characters[i].name)
+                image.addClass("enemies")
+                $("#enemiesChar").append(image)
+                console.log(enemies);
+            }
+        }
+    });
+
+        
+    $(".enemies").on("click", function () {
+        console.log("Hello");
+        // var charName1 = $(this).attr("data-name");
+        // console.log(charName1);
+        // for (var i = 0; i < characters.length; i++) {
+        //     if (characters[i].name === charName1) {
+        //         var image = $("<img>")
+        //         image.attr("src", characters[i].img)
+        //         image.addClass("attacker")
+        //         $("#attacker").append(image)
+        //         attacker= characters[i];
+        //         console.log(attacker);
+        //     }
+        // }
+    });
 }
-obi();
-
-
-
